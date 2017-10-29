3057c71d719c38520e15c4137aa9ddbf590061ea
JuniperPhoton <dengweichao@hotmail.com>
2017-10-29 22:50:01 +0800

    Temp: build xaml light

---
+++ MyerSplashCustomControl/Control/AmbLight.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace MyerSplashCustomControl.Control
+{
+    public class AmbLight : XamlLight
+    {
+        private static readonly string Id = typeof(AmbLight).FullName;
+
+        protected override void OnConnected(UIElement newElement)
+        {
+            Compositor compositor = Window.Current.Compositor;
+
+            // Create AmbientLight and set its properties
+            AmbientLight ambientLight = compositor.CreateAmbientLight();
+            ambientLight.Color = Colors.White;
+            ambientLight.Intensity = 0.5f;
+
+            // Associate CompositionLight with XamlLight
+            CompositionLight = ambientLight;
+
+            // Add UIElement to the Light's Targets
+            AmbLight.AddTargetElement(GetId(), newElement);
+        }
+
+        protected override void OnDisconnected(UIElement oldElement)
+        {
+            // Dispose Light when it is removed from the tree
+            AmbLight.RemoveTargetElement(GetId(), oldElement);
+            CompositionLight.Dispose();
+        }
+
+        protected override string GetId()
+        {
+            return Id;
+        }
+    }
+}
--- MyerSplashCustomControl/Control/MaterialCompatGrid.cs
+++ MyerSplashCustomControl/Control/MaterialCompatGrid.cs
@@ -11,7 +11,7 @@ namespace MyerSplashCustomControl.Control
         Acrylic
     }
 
-    public class AcrylicCompatGrid : Grid
+    public class MaterialCompatGrid : Grid
     {
         public BackdropType BackdropType
         {
@@ -21,16 +21,49 @@ namespace MyerSplashCustomControl.Control
 
         public static readonly DependencyProperty BackdropTypeProperty =
             DependencyProperty.Register("BackdropType",
-                typeof(BackdropType), typeof(AcrylicCompatGrid),
+                typeof(BackdropType), typeof(MaterialCompatGrid),
                 new PropertyMetadata(BackdropType.Acrylic, (s, e) =>
                 {
-                    var control = s as AcrylicCompatGrid;
+                    var control = s as MaterialCompatGrid;
                     control.UpdateBackground();
                 }));
 
-        public AcrylicCompatGrid()
+        public bool LightEnabled
+        {
+            get { return (bool)GetValue(LightEnabledProperty); }
+            set { SetValue(LightEnabledProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LightEnabled.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LightEnabledProperty =
+            DependencyProperty.Register("LightEnabled", typeof(bool),
+                typeof(MaterialCompatGrid), new PropertyMetadata(false, (s, e) =>
+                {
+                    var control = s as MaterialCompatGrid;
+                    if ((bool)e.NewValue)
+                    {
+                        control.UpdateLight();
+                    }
+                }));
+
+        private XamlLight _hoverLight;
+
+        public MaterialCompatGrid()
         {
             UpdateBackground();
+        }
+
+        private void UpdateLight()
+        {
+            if (_hoverLight == null)
+            {
+                _hoverLight = new HoverLight();
+            }
+            if (!Lights.Contains(_hoverLight))
+            {
+                Lights.Add(_hoverLight);
+                Lights.Add(new AmbLight());
+            }
         }
 
         private void UpdateBackground()
