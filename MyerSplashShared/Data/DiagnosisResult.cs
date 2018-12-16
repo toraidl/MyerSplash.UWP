e1ff54953079fbdd8fd086135e135f2486915e65
juniperphoton <dengweichao@hotmail.com>
2018-12-16 20:26:46 +0800

    Make network diagnosis more specified

---
+++ MyerSplashShared/Data/DiagnosisResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+
+namespace MyerSplashShared.Data
+{
+    public class DiagnosisResult
+    {
+        public string BriefMesasge { get; set; }
+
+        public HttpResponseMessage Response { get; set; }
+
+        public DiagnosisResult(string message, HttpResponseMessage response)
+        {
+            BriefMesasge = message;
+            Response = response;
+        }
+    }
+}
